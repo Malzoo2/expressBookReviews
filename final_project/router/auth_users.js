85e505c14b401c2e13af61e45c42bbe568c916ac
Mahmoud <Malzoo2@hotmail.com>
2025-01-29 14:45:44 +0300

    add auth functionality

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,35 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+return true;
 }
+
+
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+let validUser = users.filter((user) => {
+  return (user.username === username && user.password === password);
+});
+retrun (validUser.length> 0)?  true : false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password)
+    return res.status(404).json({message: "body issue in login"});
+
+
+  if (authenticatedUser(username,password)){
+      let accessTkn = jwt.sign({data: password},'access',{expiresIn: 60 });
+      req.session.authorization = {accessTkn: username};
+      return res.status(200).send("User Logged in ");
+  } else
+    return res.status(208).json({message: "invalid Login. check user name and password in array"});
 });
 
 // Add a book review
@@ -24,6 +43,8 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
+
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
